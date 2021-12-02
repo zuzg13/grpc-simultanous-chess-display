2589216c09c7263a8f5ee187f752a78ed3640162
zuzg13 <nanaxoxo33@gmail.com>
2021-12-02 11:19:01 +0100

    reformat file

--- src/components/GameInfo.js
+++ src/components/GameInfo.js
@@ -1,7 +1,14 @@
 import React, { useEffect, useState } from 'react'
-import {Alert} from 'react-bootstrap'
+import {Alert, Button} from 'react-bootstrap'
 
 export default function GameInfo ({info}){
+
+    const RefreshButton = () =>{
+        if(info.heading === "Oczekiwanie na pozostałych graczy")
+            return <Button variant='outline-primary' onClick={window.location.reload}>Odśwież</Button>
+        else
+            return null;
+    }
 
     return <div>
         <Alert variant="primary">
@@ -13,6 +20,7 @@ export default function GameInfo ({info}){
             <p className="mb-0">
                 {info.bodyunderline}
             </p>
+            <RefreshButton/>
         </Alert>
     </div>
 }
