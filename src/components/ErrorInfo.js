3c51d06a0b7b32b8f706ad32441d935dca65b344
zuzg13 <nanaxoxo33@gmail.com>
2021-12-28 13:22:03 +0100

    add new error code handling

--- src/components/ErrorInfo.js
+++ src/components/ErrorInfo.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import {Alert} from 'react-bootstrap'
+import React from 'react'
+import {Alert, Button} from 'react-bootstrap'
 
 const  ErrorInfo  = ({err}) => {
     try{
@@ -12,14 +12,13 @@ const  ErrorInfo  = ({err}) => {
             </Alert>;
         }
         else if (err === 9){
-            // if(err === 'Game\'s players list is full'){
-                return <Alert variant="warning">
+             return <Alert variant="warning">
                     <Alert.Heading>Rozgrywka nie ma wolnych miejsc</Alert.Heading>
                     <p>
                         Nie możesz do niej dołączyć.
                     </p>
                 </Alert>;
-            // }
+
         }
         else if (err === 6){
             return <Alert variant="warning">
@@ -34,6 +33,14 @@ const  ErrorInfo  = ({err}) => {
                 <Alert.Heading>Już jesteś dodany do tej rozgrywki</Alert.Heading>
                 <p>
                     Nie możesz do niej dołączyć.
+                </p>
+            </Alert>;
+        }
+        else if (err === 127){
+            return <Alert variant="danger">
+                <Alert.Heading>Zostałeś wylogowany z nieznanego powodu</Alert.Heading>
+                <p>
+                    <a href='../'><Button variant='outline-danger'>Zaloguj się na nowo</Button></a>
                 </p>
             </Alert>;
         }
