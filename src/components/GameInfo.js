f563afde457e74d50fe1a36692953374cea699ab
zuzg13 <nanaxoxo33@gmail.com>
2021-12-28 13:22:23 +0100

    add new game info formatting

--- src/components/GameInfo.js
+++ src/components/GameInfo.js
@@ -1,26 +1,45 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import {Alert, Button} from 'react-bootstrap'
 
 export default function GameInfo ({info}){
 
+    function refreshPage() {
+        window.location.reload(false);
+    }
+
     const RefreshButton = () =>{
         if(info.heading === "Oczekiwanie na pozostałych graczy")
-            return <Button variant='outline-primary' onClick={window.location.reload}>Odśwież</Button>
+            return <Button variant='outline-primary' onClick={refreshPage}>Odśwież</Button>
         else
             return null;
     }
 
-    return <div>
-        <Alert variant="primary">
-            <Alert.Heading>{info.heading}</Alert.Heading>
-            <p>
-                {info.body}
-            </p>
-            <hr />
-            <p className="mb-0">
-                {info.bodyunderline}
-            </p>
-            <RefreshButton/>
-        </Alert>
-    </div>
+    if(info.heading === "Koniec gry")
+        return <div>
+            <Alert variant="secondary">
+                <Alert.Heading>{info.heading}</Alert.Heading>
+                <p>
+                    {info.body}
+                </p>
+                <hr />
+                <p className="mb-0">
+                    {info.bodyunderline}
+                </p>
+                <RefreshButton/>
+            </Alert>
+        </div>
+    else
+        return <div>
+            <Alert variant="primary">
+                <Alert.Heading>{info.heading}</Alert.Heading>
+                <p>
+                    {info.body}
+                </p>
+                <hr />
+                <p className="mb-0">
+                    {info.bodyunderline}
+                </p>
+                <RefreshButton/>
+            </Alert>
+        </div>
 }
